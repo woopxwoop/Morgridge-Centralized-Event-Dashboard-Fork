92d2a2209142bac19bdc9e0ccf62259751f5b7c9
An Lou <louan1734@gmail.com>
2026-02-19 15:14:26 -0600

    add day route

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- This is the home page (Calendar View) -->
+
+<h1>[write html here]</h1>
---
+++ frontend/src/routes/day/+page.svelte
@@ -0,0 +1,3 @@
+<!-- This is the page for a specific day -->
+
+<h1>[write html here]</h1>
