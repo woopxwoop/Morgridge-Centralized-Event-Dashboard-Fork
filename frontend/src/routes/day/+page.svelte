f9d856cfac185fd6dc58f09b8bc8bb389ddeb229
alex <chyun2005@gmail.com>
2026-02-23 21:14:14 -0600

    added brief structure and dropdown effect

--- frontend/src/routes/day/+page.svelte
+++ frontend/src/routes/day/+page.svelte
@@ -1,3 +1,81 @@
 <!-- This is the page for a specific day -->
+<script lang="ts">
+    interface EventInfo {
+        id: number;
+        eventTitle: string;
+        eventHost: string;
+        eventLocation: string;
+        eventTime: string;
+        eventDescription: string;
+        pizza: boolean;
+        arrowUp: boolean; //arrowUp-open !arrowUp-closed
+    }
 
-<h1>[hmmmmmmmmmmmmmmmm]</h1>
+    let dayEvents: EventInfo[] = $state([
+        {
+            id: 1,
+            eventTitle: "Craft Night Social",
+            eventHost: "Girls Who Code-Madison Chapter",
+            eventLocation: "Morgridge Hall Student Org Lounge (2nd floor)",
+            eventTime: "7:30 - 8:30 PM",
+            eventDescription: "Join us for a break from studying to decorate our club locker and meet other fellow members! We will have pizza 🍕 and arts/craft supplies 🖌️✂️ for you to make decorations. We hope to see you there!!",
+            pizza: true,
+            arrowUp : false,
+        },
+        {
+            id: 2,
+            eventTitle: "WebTalks",
+            eventHost: "Weblabs",
+            eventLocation: "Morgridge Hall Rm. 2556",
+            eventTime: "6:00pm - 7:00pm",
+            eventDescription: "WebLabs is hosting a workshop with a frontend engineer from Fetch on publishing a website using SvelteKit and GitHub Actions. Whether you're a frontend warrior or just interested in standing up a portfolio site for your resume, this workshop will have something for everyone! Like usual for WebLabs, this event will feature pizza 🍕",
+            pizza: true,
+            arrowUp : false,
+        }
+    ])
+
+    function flipArrow(clickEvent: EventInfo): void {
+        clickEvent.arrowUp = !clickEvent.arrowUp;
+    }
+</script>
+
+<div class="schedule-container">
+  
+  {#each dayEvents as event (event.id)}
+    <div class="event-card">
+      
+      <button class="event-header" type="button" onclick={() => flipArrow(event)}>
+        <div class="header-text">
+          <h2 class="title">{event.eventTitle} {event.eventLocation} {event.eventTime}</h2>
+          <p class="time"></p>
+        </div>
+        <span class="arrow {event.arrowUp ? 'rotate' : ''}">v</span>
+      </button>
+
+      {#if event.arrowUp}
+        <div class="event-details">
+          <p class="host">{event.eventHost}</p>
+          <p class="description">{event.eventDescription}</p>
+        </div>
+      {/if}
+
+    </div>
+  {/each}
+  
+</div>
+
+<style>
+    .event-header {
+        gap: 10px;
+        display: flex;
+    }
+
+    span {
+        transition: 0.1s;
+        display: inline-block;
+    }
+
+    .rotate {
+        transform: rotate(180deg);
+    }
+</style>
